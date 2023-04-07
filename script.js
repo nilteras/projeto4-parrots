ab8f4fff576a2c2ff3b77ce4377ff8dbb5038723
nilteras <niltonrnascimento@hotmail.com>
2023-04-07 08:26:08 -0300

    versão com embaralhar e distribuir cartas

--- script.js
+++ script.js
@@ -21,6 +21,31 @@ function quantasCartas(){
 }
 quantasCartas();
 abrirBaralho();
+distribuirCartas();
+
+function distribuirCartas(){
+    const jogo = document.querySelector('.container');
+
+    for(let i=0; i<baralho.length; i++){
+        let carta = `
+        <li class="carta">
+            <div  class="front-face face">
+                <img src="./Imagens/back.png" alt="">
+            </div>
+            <div class="back-face face">
+                <img src="./Imagens/${baralho[i]}.gif">
+            </div> 
+         </li>`;
+         
+         jogo.innerHTML += carta;
+
+    }
+
+}
+
+function comparador(){
+    return Math.random() - 0.5;
+}
 
 function abrirBaralho(){
     
@@ -30,4 +55,16 @@ function abrirBaralho(){
         baralho.push(carta);
     }
     
+     baralho.sort(comparador);
+    
+}
+
+function clicar(front){
+    front = document.querySelector('.front-face');
+    console.log(front);
+    front.classlist.add('virar')
+    front.classlist.remove('face');
+    console.log(front);
+
+
 }
